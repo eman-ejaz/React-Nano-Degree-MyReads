a61923518f63d713d097ad904a478ad5f1fa4cf3
Muhammad Eman Ejaz <muhammad.eman@arbisoft.com>
2022-02-22 22:41:48 +0500

    feat: Add component for book status dropdown

---
+++ src/components/ShelfChanger.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import * as PropTypes from 'prop-types';
+
+const ShelfChanger = ({ changeBookShelf, currentShelf }) => {
+  const shelfChangeHandler = (event) => {
+    changeBookShelf(event.target.value);
+  };
+  return (
+    <div className='book-shelf-changer'>
+      <select
+        onChange={shelfChangeHandler}
+        value={currentShelf ? currentShelf : 'none'}
+      >
+        <option value='move' disabled>
+          Move to...
+        </option>
+        <option value='currentlyReading'>Currently Reading</option>
+        <option value='wantToRead'>Want to Read</option>
+        <option value='read'>Read</option>
+        <option value='none'>None</option>
+      </select>
+    </div>
+  );
+};
+
+ShelfChanger.propTypes = {
+  changeBookShelf: PropTypes.func.isRequired,
+  currentShelf: PropTypes.string.isRequired,
+};
+
+export default ShelfChanger;
