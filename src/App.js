aa2715ca243e2c7331131f7ed53eacc96c6d184a
Muhammad Eman Ejaz <muhammad.eman@arbisoft.com>
2022-02-22 22:42:25 +0500

    feat: Add search functionality and routing to project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,126 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import * as BooksAPI from './BooksAPI';
 import './App.css';
+import BookShelves from './components/BookShelves';
+import Book from './components/Book';
 
-function App() {
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+
+const BooksApp = () => {
+  const [books, setBooks] = useState([]);
+  const [searchQuery, setSearchQuery] = useState('');
+  const [booksSearchResult, setBooksSearchResult] = useState([]);
+  const [mapOfIdToBooks, setMapOfIdToBooks] = useState(new Map());
+
+  useEffect(() => {
+    BooksAPI.getAll().then((response) => {
+      setBooks(response);
+      setMapOfIdToBooks(createMapOfBooks(response));
+    });
+  }, []);
+
+  useEffect(() => {
+    if (searchQuery)
+      BooksAPI.search(searchQuery).then((response) => {
+        if (response.error) setBooksSearchResult([]);
+        else {
+          const searchBooks = response.map((book) => {
+            books.filter((stateBook) => {
+              if (stateBook.id === book.id) {
+                book.shelf = stateBook.shelf;
+              }
+              return book;
+            });
+            return book;
+          });
+          setBooksSearchResult(searchBooks);
+        }
+      });
+    else setBooksSearchResult([]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [searchQuery]);
+
+  const createMapOfBooks = (books) => {
+    const map = new Map();
+    books.map((book) => map.set(book.id, book));
+    return map;
+  };
+
+  const bookShelfChangeHandler = (bookData) => {
+    let updatedBooks = books.map((book) => {
+      if (book.id === bookData.book.id) {
+        book.shelf = bookData.newShelf;
+        return book;
+      }
+      return book;
+    });
+    if (!mapOfIdToBooks.has(bookData.book.id)) {
+      bookData.book.shelf = bookData.newShelf;
+      updatedBooks.push(bookData.book);
+    }
+    setBooks(updatedBooks);
+    BooksAPI.update(bookData.book, bookData.newShelf);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <Router>
+        <Switch>
+          <Route path='/search'>
+            <div className='search-books'>
+              <div className='search-books-bar'>
+                <Link to='/'>
+                  <button className='close-search'>Close</button>
+                </Link>
+
+                <div className='search-books-input-wrapper'>
+                  <input
+                    type='text'
+                    placeholder='Search by title or author'
+                    onChange={(e) => setSearchQuery(e.target.value)}
+                    value={searchQuery}
+                  />
+                </div>
+              </div>
+              <div className='search-books-results'>
+                <ol className='books-grid'>
+                  {booksSearchResult.map((book) => (
+                    <li key={book.id}>
+                      <Book
+                        bookData={book}
+                        changeBookShelf={bookShelfChangeHandler}
+                      />
+                    </li>
+                  ))}
+                </ol>
+              </div>
+            </div>
+          </Route>
+          <Route path='/'>
+            <div className='list-books'>
+              <div className='list-books-title'>
+                <h1>MyReads</h1>
+              </div>
+
+              <div className='list-books-content'>
+                <div>
+                  <BookShelves
+                    books={books}
+                    changeBookShelf={bookShelfChangeHandler}
+                  />
+                </div>
+              </div>
+              <div className='open-search'>
+                <Link to='/search'>
+                  <button>Add a book</button>
+                </Link>
+              </div>
+            </div>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
-}
+};
 
-export default App;
+export default BooksApp;
---
+++ src/constants.js
@@ -0,0 +1,15 @@
+export const CURRENTLY_READING_SHELF = 'currentlyReading';
+export const WANT_TO_READ_SHELF = 'wantToRead';
+export const READ_SHELF = 'read';
+
+export const shelfTitles = {
+  currentReading: 'Currently Reading',
+  wantToRead: 'Want To Read',
+  read: 'Read',
+};
+
+export const shelfIds = {
+  currentReading: 'currentlyReading',
+  wantToRead: 'wantToRead',
+  read: 'read',
+};
